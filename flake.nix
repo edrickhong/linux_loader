26b830e0a5c42c90c1af631e0367a3ad9cd74565
ehzx <edrickhong@gmail.com>
2025-05-22 20:47:18 -0400

    emulating parts of the standard linux file heirarchy for testing

--- flake.nix
+++ flake.nix
@@ -8,21 +8,36 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+
+        fakeLibPath = pkgs.runCommand "fake-lib-dir" {} ''
+          mkdir -p $out/lib
+          cp ${pkgs.glibc}/lib/libc.so.6 $out/lib/
+          cp ${pkgs.glibc}/lib/ld-linux-x86-64.so.2 $out/lib/
+          cp ${pkgs.zlib}/lib/libz.so.1 $out/lib/
+          cp ${pkgs.libffi}/lib/libffi.so.8 $out/lib/
+        '';
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             cmake
-            clang  # or clang
-	    gdb
+            clang
+            gdb
             cgdb
-	    
-	    dbus
-	    pkg-config
-	    alsa-lib
-	    xorg.libX11
-	    wayland
-	    libffi
+
+            dbus
+            pkg-config
+            alsa-lib
+            xorg.libX11
+            wayland
+            libffi
           ];
+
+          shellHook = ''
+            export LD_LIBRARY_PATH=${fakeLibPath}/lib
+            export LD_BIND_NOW=1
+            export LD_PRELOAD=
+            echo '[ld.so emu] LD_LIBRARY_PATH=$LD_LIBRARY_PATH'
+          '';
         };
       });
 }
